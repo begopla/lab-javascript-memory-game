20837d1a73028a1f1d5f8d171c283546e39eb6c2
begopla <begonapr@gmail.com>
2022-03-21 12:46:11 +0400

    Solved lab

--- js/index.js
+++ js/index.js
@@ -27,6 +27,40 @@ const cards = [
 
 const memoryGame = new MemoryGame(cards);
 
+let numberOfClicks=0;
+let cardArray=[];
+//TODO pick the pairs click to updat the numbers
+const pairsClicked = document.getElementById('pairs-clicked')
+
+const pairsGuessed = document.getElementById('pairs-guessed')
+
+let counterPairsGuessed=0;
+let counterPairsClicked =0;
+let cardNames =[];
+// console.log(pairsClicked);
+function checkResults (cardArray) {
+
+  cardArray.forEach((card) =>{
+    const name = card.getAttribute('data-card-name')//gets the name of the attribute 
+    cardNames.push(name)
+  })
+  const resultIsPaired = memoryGame.checkIfPair(cardNames[0], cardNames[1])  
+  return resultIsPaired;
+}
+
+function removeTurned(array) {
+  setTimeout(()=>{
+    array.classList.remove('turned')        
+  },5000)
+}
+function gameOver (){
+const gameOver= memoryGame.checkIfFinished()
+  if(gameOver){
+    prompt('You are awesome, you fisihed the game!')
+    
+  }
+}
+memoryGame.shuffleCards(); //* shuffle the cards at the beguining
 window.addEventListener('load', (event) => {
   let html = '';
   memoryGame.cards.forEach((pic) => {
@@ -37,15 +71,88 @@ window.addEventListener('load', (event) => {
       </div>
     `;
   });
+ 
 
   // Add all the divs to the HTML
   document.querySelector('#memory-board').innerHTML = html;
+  let readyToPlay = prompt("Remember the cards in 5 seconds and play!");
+   // Bind the click event of each element to a function
 
-  // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach((card) => {
-    card.addEventListener('click', () => {
-      // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
-    });
+
+    card.classList.add('turned')
+    //
+   //if(readyToPlay === "Y" || readyToPlay === "y" || readyToPlay === "Yes" || readyToPlay === "yes"){
+
+    removeTurned(card) //after the timer it turns back all cards
+      //prompt('GAME ON! REMEMBER ALL CARDS?')
+      
+      card.addEventListener('click', () => {
+        // TODO: write some code here
+        numberOfClicks +=1;
+        //console.log(`Card clicked: ${card}`);
+        cardArray.push(card)
+        card.classList.add('turned')
+        
+        if(numberOfClicks===2){
+          cardNames =[]
+          const resultIsPaired = checkResults(cardArray);
+        
+         setTimeout(() => {
+           
+           if(resultIsPaired){
+             counterPairsGuessed++;
+             pairsGuessed.innerHTML = counterPairsGuessed;
+             prompt('Congratulations, you got it right!')
+             gameOver();
+            }     
+
+            else {prompt(`You have picked the wrong cards, please try again`);
+            counterPairsClicked ++;
+            pairsClicked.innerHTML = counterPairsClicked;
+            cardArray.forEach(card => {
+              card.classList.remove('turned') 
+            })
+                 }
+         }, 500);
+        }
+        
+        
+        ///! While loop to continue with game until finished.
+        while(((numberOfClicks/2)>1 && 1>counterPairsGuessed<(cards.length/2))){
+          numberOfClicks = 1;
+          cardArray=[]
+          card.classList.add('turned')
+          cardArray.push(card)
+          
+          
+          if(numberOfClicks===2){
+            const resultIsPaired = checkResults(cardArray);
+            setTimeout(() => {
+              
+              if(resultIsPaired){
+                counterPairsGuessed++;
+                pairsGuessed.innerHTML = counterPairsGuessed;
+                prompt('Congratulations, you got it right!')
+                gameOver();
+              }       
+              else {prompt(`You have picked the wrong cards, please try again`);
+            counterPairsClicked ++;
+            pairsClicked.innerHTML = counterPairsClicked;
+            
+            cardArray.forEach(card => {
+              card.classList.remove('turned')
+              cardArray = [];
+              
+            })
+          }
+         }, 1000);
+        }
+        }
+      });
+    
+    //}
+
   });
+  
 });
--- js/memory.js
+++ js/memory.js
@@ -1,21 +1,51 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked=0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
-    // ... write your code here
+    // for i from n−1 downto 1 do
+    //  j ← random integer such that 0 ≤ j ≤ i
+    //  exchange a[j] and a[i]
+    if(!this.cards){
+     return //* Same as returning undefined /
+     //return undefined    
+    }
+    
+    for(let i=0; i<(this.cards.length);i++){
+      const randomCardIndex = Math.floor(Math.random()*this.cards.length);
+      const randomCard = this.cards[randomCardIndex];
+      
+      this.cards[randomCardIndex]=this.cards[i];
+      this.cards[i]=randomCard;
+      
+    }
+   // this.cards=shuffleArray; //* Assign the shuffled cards into the cards array
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    if(card1 === card2){
+      this.pairsGuessed ++;
+      return true;
+    }
+    else {
+      this.pairsClicked ++;
+      return false;}
   }
 
   checkIfFinished() {
-    // ... write your code here
+   if(this.pairsGuessed === (this.cards.length/2)){
+     return true;
+   }
+   else 
+     return false;
+   
+   }
   }
-}
+
 
 // The following is required for automated testing. Please, ignore it.
 if (typeof module !== 'undefined') module.exports = MemoryGame;
